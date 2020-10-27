a9cf8922ac4949b2abe291b347c67535679e2568
Brett Taylor <batzerk@gmail.com>
2020-10-27 12:05:17 +0100

    +Basics for menus

---
+++ Assets/Scripts/Menus/BasePanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetVisibility(bool _isVisible)
+    {
+        this.gameObject.SetActive(_isVisible);
+    }
+}
---
+++ Assets/Scripts/Menus/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    // References
+    [SerializeField] private BasePanel panel_IBs=null;
+    [SerializeField] private BasePanel panel_today=null;
+
+
+    // ================================================================
+    //  Start
+    // ================================================================
+    void Start()
+    {
+        ShowPanel(panel_today);
+    }
+
+
+    // ================================================================
+    //  Doers
+    // ================================================================
+    public void ShowPanel(BasePanel _panel)
+    {
+        panel_IBs.SetVisibility(false);
+        panel_today.SetVisibility(false);
+
+        _panel.SetVisibility(true);
+    }
+
+    // ================================================================
+    //  Events
+    // ================================================================
+    public void OpenPanel_IBs() { ShowPanel(panel_IBs); }
+    public void OpenPanel_Today() { ShowPanel(panel_today); }
+
+
+
+}
